be9d06ebcf440b96f0b19391aab56c86d84083bd
Ferdysan <ferdysan87@gmail.com>
2019-05-06 15:09:23 +0200

    inizio

---
+++ main.js
@@ -0,0 +1,52 @@
+// - Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+// - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+// - Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+//creo un oggetto studente
+var studente ={
+  nome:'Ferdinando',
+  cognome: 'Martino',
+  età: 31
+}
+
+//stampo le proprietà di studente
+for (var field in studente) {
+  document.writeln(studente[field]);
+}
+
+//creo un array di oggetti
+var studenti =[
+  {
+    nome:'Armando',
+    cognome: 'vallante',
+    età: 19
+  },
+  {
+    nome:'Lucia',
+    cognome: 'Martino',
+    età: 20
+  },
+  {
+    nome:'Pasquale',
+    cognome: 'Sasso',
+    età: 25
+  }
+]
+
+//stampo l'array con gli studenti
+for( var i = 0; i<studenti.length; i++){
+  document.writeln('Nome : ' + studenti[i].nome + ' Cognome : ' + studenti[i].cognome + ' età :  ' + studenti[i].età + '</br>' );
+}
+
+
+//creo dei prompt per inserire un nuovo oggetto
+
+var nome_utente= prompt('Inserisci un nome ');
+
+var cognome_utente= prompt('Inserisci un cognome ');
+
+var età_utente= parseInt(prompt('inserisci la sua età'));
+
+studenti.push(nome_utente, cognome_utente, età_utente);
+
+console.log(studenti);
